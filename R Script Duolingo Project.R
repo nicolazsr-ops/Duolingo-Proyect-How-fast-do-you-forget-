8308ff024b4978cc59dd75a345d2e79b92287ca6
Chokis1up <chocontajd1904@gmail.com>
2026-09-08 17:30:15 -0500

    Update R Script Duolingo Project.R

--- R Script Duolingo Project.R
+++ R Script Duolingo Project.R
@@ -2,8 +2,10 @@
 
 ## Notas
 
-# Por el momento solo lo he organizado. Dejo en comentarios abajo la estructura que pensaba seguir, dime que opinas. 
 # La carga de datos la puse en comentarios, para cuando la necesites.
+# Revisé el trabajo que teniamos y necesitamos 3 variables númericasy una categorica para explicar otra númerica.
+# Mi propuesta es utilizar lag_days, history_seen y history_correct para explicar p_recall, y como categórica usar . Ignorar la existencia 
+# Los histogramas no dan mucha información por si solos
 
 
 ## Librerias
@@ -17,15 +19,20 @@ library(ggplot2)              #Diagramas de frecuencia
 
 #datos_word_difficulty <- read.csv("C:\\Users/nisan/OneDrive - ESCUELA COLOMBIANA DE INGENIERIA JULIO GARAVITO/MAESTRÍA CIENCIA DE DATOS/MODELOS ESTADÍSTICOS/PROYECTO/word_difficulty.csv")
 #datos_learning_traces_sample <- read.csv("C:\\Users/nisan/OneDrive - ESCUELA COLOMBIANA DE INGENIERIA JULIO GARAVITO/MAESTRÍA CIENCIA DE DATOS/MODELOS ESTADÍSTICOS/PROYECTO/learning_traces_sample.csv")
-#datos_word_difficulty <- read.csv("D:\\Downloads/word_difficulty.csv")
-#datos_learning_traces_sample <- read.csv("D:\\Downloads/learning_traces_sample.csv")
-
+datos_word_difficulty <- read.csv("D:\\Downloads/word_difficulty.csv")
+datos_learning_traces_sample <- read.csv("D:\\Downloads/learning_traces_sample.csv")
+datos_forgetting <- read.csv("D:\\Downloads/forgetting_curve.csv")
 
 ## Visulización de los datos
- 
+
 View(datos_word_difficulty)
 View(datos_learning_traces_sample)
+View(datos_forgetting)
 # Para revisar los primeros datos: head(datos_word_difficulty)  (dado por GPT)
+
+## Variable Objetivo
+#   p_recall:  Porcentaje de aciertos sobre apariciones de la palabra en la sesión
+#descr(datos_learning_traces_sample$p_recall)
 
 
 ## Variables Númericas
@@ -56,8 +63,47 @@ descr(datos_learning_traces_sample$lag_days)
 
 ## Histogramas
 
-#hist(datos_learning_traces_sample$ui_language) (arreglar)
-# Completar
+#history_seen
+hist(datos_learning_traces_sample$history_seen,
+     main = "Distribución de Frecuencia: Visualizaciones previas", 
+     xlab = "Visualizaciones previas", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
+
+#history_correct
+hist(datos_learning_traces_sample$history_correct,
+     main = "Distribución de Frecuencia: Visualizaciones correctas", 
+     xlab = "Visualizaciones correctas", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
+
+#session_seen
+hist(datos_learning_traces_sample$session_seen,
+     main = "Distribución de Frecuencia: Visualizaciones en la última sesión", 
+     xlab = "Visualizaciones en la última sesión", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
+
+#session_correct
+hist(datos_learning_traces_sample$session_correct,
+     main = "Distribución de Frecuencia: Visualizaciones correctas\n en la última sesión", 
+     xlab = "Visualizaciones correctas en la última sesión", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
+
+#lag_days
+hist(datos_learning_traces_sample$lag_days,
+     main = "Distribución de Frecuencia: Dias tras la última sesión", 
+     xlab = "Dias tras la última sesión", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
+
+#p_recall
+hist(datos_learning_traces_sample$p_recall,
+     main = "Distribución de Frecuencia: Dias tras la última sesión", 
+     xlab = "Dias tras la última sesión", 
+     ylab = "Frecuencia", 
+     col = "lightblue")
 
 
 ## Gráficas Distribuciones de Frecuencia
@@ -98,4 +144,3 @@ ggplot(datos_learning_traces_sample, aes(x = lag_days)) +
 
 # Coeficiente Pearson
 # Coeficiente Spearman
-
